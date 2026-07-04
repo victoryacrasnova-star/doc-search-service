1e992a3aad91ea5d0c535073829aa52c61919f84
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-07-04 04:11:18 +0300

    update loader

---
+++ app/loader.py
@@ -0,0 +1,48 @@
+import csv
+from pathlib import Path
+from app.models import Document
+
+import ast
+from datetime import datetime
+
+from app.database import SessionLocal
+
+CSV_PATCH = Path("data/posts.csv")
+
+def parse_rubrics(value: str) -> str:
+    rubrics = ast.literal_eval(value)
+    return ", ".join(rubrics)
+
+
+def parse_created_date(value: str) -> datetime:
+    return datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+
+def load_csv():
+    db = SessionLocal()
+    try:
+        with CSV_PATCH.open("r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                rubrics = parse_rubrics(row["rubrics"])
+                created_date = parse_created_date(row["created_date"])
+
+                print(rubrics)
+                print(type(rubrics))
+
+                print(created_date)
+                print(type(created_date))
+
+                document = Document(
+                    rubrics=rubrics,
+                    text=row["text"],
+                    created_date=created_date,
+                )
+                db.add(document)
+            db.commit()
+    finally:
+        db.close()
+        
+
+if __name__ == "__main__":
+    load_csv()
